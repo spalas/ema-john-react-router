3d7a695c4c2bbf9d47e902a8cde3f205eeacb19a
palash sarkar <spalash521@gmail.com>
2021-10-01 12:03:49 +0300

    last all added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>404</h2>
+            <h3>The page you are looking for i not available</h3>
+        </div>
+    );
+};
+
+export default NotFound;
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import useProducts from '../../hooks/UseProducts';
+import useCart from '../../hooks/UseCart'
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb'
 
 const OrderReview = () => {
+
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        removeFromDb(key);
+
+
+
+
+    }
     return (
-        <div>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+
+                    ></ReviewItem>)
+                }
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+
+
+            </div>
+
+
+
+
+
+
+            {/* <h1>{products.length}</h1>
+            <h1>{cart.length}</h1>
+
             <h2>This is Order Review</h2>
+            <Cart cart={cart}></Cart> */}
         </div>
     );
 };
